f41a76163c722c5f24ab746fe8cf1540d7dc8c12
mahati21 <mahatigr21@gmail.com>
2026-03-21 21:33:24 +0530

    Feature UC2 : Display Hello with command-line argument

--- src/main/java/com/helloapp/HelloAppJava.java
+++ src/main/java/com/helloapp/HelloAppJava.java
@@ -1,7 +1,16 @@
 package com.helloapp;
 
+import java.util.Scanner;
+
 public class HelloAppJava {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+
+        System.out.println("Hello, " + name + "!");
+
     }
 }
