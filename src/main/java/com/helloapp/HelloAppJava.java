a7314a2f47e78bd709432b745fe9efc80ccb3798
mahati21 <mahatigr21@gmail.com>
2026-03-21 21:46:10 +0530

    Feature UC3 : Display Hello or default message

--- src/main/java/com/helloapp/HelloAppJava.java
+++ src/main/java/com/helloapp/HelloAppJava.java
@@ -1,16 +1,12 @@
 package com.helloapp;
 
-import java.util.Scanner;
-
 public class HelloAppJava {
     public static void main(String[] args) {
-
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Enter your name: ");
-        String name = scanner.nextLine();
+        String name = "World";
+        if (args.length > 0) {
+            name = args[0];
+        }
 
         System.out.println("Hello, " + name + "!");
-
     }
 }
