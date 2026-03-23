beefee26108ec0b9ebbf8525a88aef911d46231a
mahati21 <mahatigr21@gmail.com>
2026-03-23 14:37:32 +0530

    Feature UC5 : Display Hello with enhanced loop

--- src/main/java/com/helloapp/HelloAppJava.java
+++ src/main/java/com/helloapp/HelloAppJava.java
@@ -3,17 +3,20 @@ package com.helloapp;
 public class HelloAppJava {
     public static void main(String[] args) {
 
-        String message;
-
-        if (args.length > 0) {
-
-            String names = String.join(", ", args);
-            message = "Hello, " + names + "!";
-        } else {
-
-            message = "Hello, World!";
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+            return;
         }
 
-        System.out.println(message);
+        StringBuilder names = new StringBuilder();
+
+        for (String name : args) {
+            if (names.length() > 0) {
+                names.append(", ");
+            }
+            names.append(name);
+        }
+
+        System.out.println("Hello, " + names + "!");
     }
 }
