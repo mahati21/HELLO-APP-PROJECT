547636f34772ea123ad6f277da47aa337999929e
mahati21 <mahatigr21@gmail.com>
2026-03-29 12:05:42 +0530

    Feature UC7: Display Hello using StringJoin()

--- src/main/java/com/helloapp/HelloAppJava.java
+++ src/main/java/com/helloapp/HelloAppJava.java
@@ -3,19 +3,15 @@ package com.helloapp;
 public class HelloAppJava {
     public static void main(String[] args) {
 
+        String greeting;
+
         if (args.length == 0) {
-            System.out.println("Hello, World!");
-            return;
+            greeting = "Hello, World!";
+        } else {
+            String names = String.join(", ", args);
+            greeting = "Hello, " + names + "!";
         }
 
-        StringBuilder names = new StringBuilder();
-
-        for (String name : args) {
-            if (names.length() > 0) {
-                names.append(", ");
-            }
-            names.append(name);
-        }
-        System.out.println("Hello, " + names + "!");
+        System.out.println(greeting);
     }
 }
